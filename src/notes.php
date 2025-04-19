7836e42dbb2c9ea3041a5fb7f1f1318df0c95df8
HabiburRahman47 <habiburkuetacbd@gmail.com>
2025-04-19 17:04:13 +0600

    addeddeploy process

--- src/notes.php
+++ src/notes.php
@@ -1,5 +1,4 @@
 <?php
     echo "<h2>Here you'll write your notes.</h2>";
-    echo "<p>Click on the <strong>Notes</strong> tab to add your notes.<p>"
-    79w^sf
+    echo "<p>Click on the <strong>Notes</strong> tab to add your notes.<p>";
 ?>
