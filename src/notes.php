5095c4a0f24939d35891dcaf6742d3f662d27756
HabiburRahman47 <habiburkuetacbd@gmail.com>
2025-04-19 12:11:02 +0600

    worked on notes.php

--- src/notes.php
+++ src/notes.php
@@ -1,4 +1,5 @@
 <?php
     echo "<h2>Here you'll write your notes.</h2>";
-    echo "<p>Click on the <strong>Notes</strong> tab to add your notes.</p>"
+    echo "<p>Click on the <strong>Notes</strong> tab to add your notes.<p>"
+    79w^sf
 ?>
