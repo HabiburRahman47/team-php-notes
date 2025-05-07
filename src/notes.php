ef9a623810785d1b52003da967f6a47578ae2bbe
HabiburRahman47 <habiburkuetacbd@gmail.com>
2025-05-07 18:30:06 +0600

    added Jenkinfile

--- src/notes.php
+++ src/notes.php
@@ -1,5 +1,5 @@
 <?php
     echo "<h2>Here you'll write your notes.</h2>";
-    <h3>New notes</h3>
-    <h4>1. Take care of your health</h4>
+    echo "<h3>New notes</h3>";
+    echo "<h4>1. Take care of your health</h4>";
 ?>
